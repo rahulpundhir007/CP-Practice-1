89979d2d27e3527d0e410fb99f8fb391aa4deb8d
rahulpundhir007 <rahulpundhir07@gmail.com>
2025-09-01 15:14:42 +0530

    Update A Spread.java

--- A Spread.java
+++ A Spread.java
@@ -1,4 +1,4 @@
- // Q 7 A Spread :
+//Q1 Netflix
 import java.util.*;
 public class Main{
   public static void main(String[] args){
@@ -16,3 +16,21 @@ public class Main{
     System.out.println(sb.toString());
   }
 }
+// Q 7 A Spread :
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i = 0;i<n.length();i++){
+      if(i==n.length()-1){
+      sb.append(n.charAt(i));
+      continue;
+    }
+    sb.append(n.charAt(i)+" ");
+    }
+    System.out.println(sb.toString());
+  }
+}        
