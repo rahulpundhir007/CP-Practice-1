afe1416f956e63848ccc33863102956b99c16003
rahulpundhir007 <rahulpundhir07@gmail.com>
2025-09-01 15:07:03 +0530

    Create A Spread.java

---
+++ A Spread.java
@@ -0,0 +1,18 @@
+ // Q 7 A Spread :
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i = 0;i<n.length();i++){
+      if(i==n.length()-1){
+      sb.append(n.charAt(i));
+      continue;
+    }
+    sb.append(n.charAt(i)+" ");
+    }
+    System.out.println(sb.toString());
+  }
+}
